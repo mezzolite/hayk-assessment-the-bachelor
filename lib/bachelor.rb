7e8b8b4b89c5f6d8713c0211773d8e2d56404ab8
mezzolite <mez.charney@gmail.com>
2019-10-09 11:23:57 -0600

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,53 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+     return contestant["name"].split[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  hometown_girls = []
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        hometown_girls << contestant["name"]
+      end
+    end
+  end
+  return hometown_girls.count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  hometown_jobs = []
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        hometown_jobs << contestant["occupation"]
+      end
+    end
+  end
+  return hometown_jobs[0]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_array = []
+  average_age = 0
+  data[season].each do |contestant|
+    age_array << contestant["age"].to_f
+  end
+  average_age = age_array.reduce(:+)/age_array.length
+  return average_age.round
 end
